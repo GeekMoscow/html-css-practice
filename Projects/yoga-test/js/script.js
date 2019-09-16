632196388f3d60c5d0c7942dbfeba10a2e2185e3
Artem Luzan <unlimitet2011@yandex.ru>
2019-09-16 13:54:59 +0300

    JS yoga timer

--- Projects/yoga-test/js/script.js
+++ Projects/yoga-test/js/script.js
@@ -1,5 +1,4 @@
 //load - только после всей загрузки //DOMContentLoaded срабатывает после загрузки DOM дерева
-
 window.addEventListener("DOMContentLoaded",function() { 
     'use strct';
     let tab = document.querySelectorAll(".info-header-tab");
@@ -36,4 +35,58 @@ window.addEventListener("DOMContentLoaded",function() {
             }
         }
     });
+
+    //timer
+    //задаем конечную. дату
+    let deadLine = '2019-10-21';
+
+    //узнать вермя сейчс и между дедлайном + вычесляем , часы, мин, сек
+    function getTimeremaining(endTime) {
+        //получаем милисикунды разницу дедлайн - сейчас
+        let t = Date.parse(endTime) - Date.parse(new Date());
+        //получаем данные из наших миллисекунд
+        //получаем остаток от минут // t/1000 - кол-во секунд
+        let seconds = Math.floor((t/1000) % 60);
+
+        let minutes = Math.floor((t/1000/60) % 60);
+
+        let hours = Math.floor(t / (1000*60*60));
+
+        let days = Math.floor((t/1000/60/60) % 24);
+        let CountDays =  Math.floor(t / (1000*60*60*24));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+
+        
+    }
+            //превращение стат верстки в динамическую
+        //принимает два аргумента
+        function setclock(id, endtime) {
+            let timer = document.getElementById(id);
+            let hour = timer.querySelector('.hours');
+            let min = timer.querySelector('.minutes');
+            let sec = timer.querySelector('.seconds');
+
+            let timeinterval = setInterval(updateClock, 1000);
+
+            function updateClock() {
+                let t = getTimeremaining(endtime);
+                hour.textContent = t.hours;
+                min.textContent = t.minutes;
+                sec.textContent = t.seconds;
+
+                if(t.total <= 0) {
+                    clearInterval(timeinterval);
+                }
+        }
+    }
+    //запускаем функцию
+    setclock('timer', deadLine);
+
+
 });
