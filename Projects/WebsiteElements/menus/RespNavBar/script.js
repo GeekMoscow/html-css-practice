c08ac6058a130919886449da280ac25528ab79d6
Artem Luzan <unlimitet2011@yandex.ru>
2019-10-10 18:47:55 +0300

    Responsive navigation

---
+++ Projects/WebsiteElements/menus/RespNavBar/script.js
@@ -0,0 +1,14 @@
+let icon = document.querySelector('.icon');
+
+
+function myFunction() {
+    let x = document.querySelector("#myTopnav");
+    if(x.className === "topnav") {
+        x.className +=' responsive';
+    } else {
+          x.className = 'topnav';
+    }
+
+}
+
+icon.addEventListener("click", myFunction);
